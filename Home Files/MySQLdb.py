617a32728af24039eb6f113073c685fa2cf59d94
Daniel Thomas <danielthomas06@hotmail.com>
2017-03-24 15:54:09 -0400

    Add files via upload

---
+++ Home Files/MySQLdb.py
@@ -0,0 +1,9 @@
+#Private database connection credentials
+
+class dbConnect:
+    def __init__(self):
+         self.host = 'localhost'
+         self.user = 'root'
+         self.password = ""   #default password is ""
+         self.db = "bridgeapp"
+
