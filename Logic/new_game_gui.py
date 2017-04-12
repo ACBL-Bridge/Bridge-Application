f5dece3390a8c6b0aaf0dd8f2bf0381d9bee523c
dualpal <jorgeb_201@hotmail.com>
2017-04-12 15:44:27 -0400

    Update new_game_gui.py

--- Logic/new_game_gui.py
+++ Logic/new_game_gui.py
@@ -8,17 +8,19 @@ from new_game_popout import *
 from game_display import *
 from create_img_item import *
 from _begin_game import *
+from start_trick_gui  import *
 from verbose import *
 from players import *
 import random
 
 # global variables
-winner = 'initial'
+#winner = 'initial'
 
 class start_gui_game(tk.Frame):
 
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self,parent, *args, **kwargs)
+        self.winner = 'initial'
         # create canvas
         self.canvas = tk.Canvas(parent, width=800, height=800, background="green")
         self.canvas.pack()
@@ -26,33 +28,40 @@ class start_gui_game(tk.Frame):
         self.new_bgame = RoundStart()
         self.img = CardImage()
         self.c = Card_Display(self.canvas, self.img)
-        self.c.current_play_display(winner, self.new_bgame.playerlst)
+        self.c.current_play_display(self.winner, self.new_bgame.playerlst)
 
-        self.canvas.tag_bind('ps', '<Button-1>', self.onObjectClick1)
-        self.canvas.tag_bind('pn', '<Button-1>', self.onObjectClick2)
-        self.canvas.tag_bind('pw', '<Button-1>', self.onObjectClick3)
-        self.canvas.tag_bind('pe', '<Button-1>', self.onObjectClick4)
+        # self.canvas.tag_bind('ps', '<Button-1>', self.onObjectClick1)
+        # self.canvas.tag_bind('pn', '<Button-1>', self.onObjectClick2)
+        # self.canvas.tag_bind('pw', '<Button-1>', self.onObjectClick3)
+        # self.canvas.tag_bind('pe', '<Button-1>', self.onObjectClick4)
         # diamond, club - for color s
         self.p = Popout(parent, self.img, self.new_bgame)
         self.p.place(relx=0.5, rely=0.5, anchor="center")
         self.p.grab_set()
+        self.p.wait_window()
+        self.winner = self.p.current_declarer
+        self.c.current_play_display(self.winner, self.new_bgame.playerlst)
+        self.t = StartTrick(self.canvas, self.winner, self.c.south_hand, self.c.west_hand, self.c.north_hand, self.c.east_hand)
 
     def onObjectClick1(self, event):
         if self.canvas.find_withtag("current"):
-            self.canvas.coords("current", 400, 450)
+            self.canvas.coords("current", 410, 430)
+            self.canvas.tag_raise("current")
 
     def onObjectClick2(self, event):
         if self.canvas.find_withtag("current"):
-            self.canvas.coords("current", 400, 350)
+            self.canvas.coords("current", 400, 370)
+            self.canvas.tag_raise("current")
 
     def onObjectClick3(self, event):
         if self.canvas.find_withtag("current"):
-            self.canvas.coords("current", 305, 400)
+            self.canvas.coords("current", 380, 410)
+            self.canvas.tag_raise("current")
 
     def onObjectClick4(self, event):
         if self.canvas.find_withtag("current"):
-            self.canvas.coords("current", 495, 400)
-
+            self.canvas.coords("current", 428, 390)
+            self.canvas.tag_raise("current")
 
 
 
