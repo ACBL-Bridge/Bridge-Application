8b4eb5cc6cd7d8212274ef3099bb130ab336dc10
dualpal <jorgeb_201@hotmail.com>
2017-04-21 08:54:08 -0400

    Update new_game_gui.py

--- Logic/new_game_gui.py
+++ Logic/new_game_gui.py
@@ -21,63 +21,31 @@ import random
 class start_gui_game(tk.Frame):
 
     def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self,parent, *args, **kwargs)
+        tk.Frame.__init__(self, parent, *args, **kwargs)
         self.winner = 'initial'
-        # create canvas
-        # self.w, self.h = parent.winfo_screenwidth(), parent.winfo_screenheight()
-        # self.canvas = tk.Canvas(parent, width=self.w, height=self.h, background="green")
         self.canvas =  tk.Canvas(parent, bg="green", highlightthickness=0)
         self.canvas.pack(fill="both", expand=True)
-        # self.canvas.pack()
 
         self.new_bgame = RoundStart()
         self.img = CardImage()
         self.c = Card_Display(self.canvas, self.img)
         self.c.current_play_display(self.winner, self.new_bgame.playerlst)
 
-        # self.canvas.tag_bind('ps', '<Button-1>', self.onObjectClick1)
-        # self.canvas.tag_bind('pn', '<Button-1>', self.onObjectClick2)
-        # self.canvas.tag_bind('pw', '<Button-1>', self.onObjectClick3)
-        # self.canvas.tag_bind('pe', '<Button-1>', self.onObjectClick4)
-        # diamond, club - for color s
-        # new code
         self.contract_Window = Popout_contract(parent, self.img)
         self.contract_Window.place(relx=0.98, rely=0.97, anchor="se")
-        # end new code
+
         self.p = Popout(parent, self.img, self.new_bgame, self.contract_Window)
         self.p.place(relx=0.5, rely=0.5, anchor="center")
         self.p.grab_set()
         self.p.wait_window()
         # Display cards base on who is the declarer
-        self.winner = self.p.current_declarer
-        print(self.winner)
-        self.c.current_play_display(self.winner, self.new_bgame.playerlst)
+        self.c.current_play_display(self.p.dummy, self.new_bgame.playerlst)
         # Create Score Table
         self.score_table = Score_Table(parent)
         self.score_table.place(relx=0.05, rely=0.97, anchor="sw")
         # Start trick
-        self.t = StartTrick(self.canvas, self.winner, self.c.south_hand, self.c.west_hand, self.c.north_hand, self.c.east_hand)
-
-    def onObjectClick1(self, event):
-        if self.canvas.find_withtag("current"):
-            self.canvas.coords("current", 410, 430)
-            self.canvas.tag_raise("current")
-
-    def onObjectClick2(self, event):
-        if self.canvas.find_withtag("current"):
-            self.canvas.coords("current", 400, 370)
-            self.canvas.tag_raise("current")
-
-    def onObjectClick3(self, event):
-        if self.canvas.find_withtag("current"):
-            self.canvas.coords("current", 380, 410)
-            self.canvas.tag_raise("current")
-
-    def onObjectClick4(self, event):
-        if self.canvas.find_withtag("current"):
-            self.canvas.coords("current", 428, 390)
-            self.canvas.tag_raise("current")
-
+        self.t = StartTrick(parent, self.canvas, self.img, self.c, self.new_bgame,  self.contract_Window,
+                            self.p.declarer, self.p.dummy, self.c.south_hand, self.c.west_hand, self.c.north_hand, self.c.east_hand)
 
 
 if __name__ == "__main__":
@@ -87,7 +55,6 @@ if __name__ == "__main__":
     # root.attributes("-fullscreen", True)
     w, h = root.winfo_screenwidth(), root.winfo_screenheight()
     root.geometry("%dx%d+0+0" % (w, h))
-    # root.geometry("800x800")
     root.title("Bridge")
     start_gui_game(root)
     root.mainloop()
